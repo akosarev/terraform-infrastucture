7631bc32fb26b77ba0c4bf671ae1487c43e86a96
Artemiy Kosarev <artemiy.kosarev@klarna.com>
2021-07-14 15:47:44 +0200

    Remove kubernetes from infrastructure

--- main.tf
+++ main.tf
@@ -12,21 +12,6 @@ provider "aws" {
 }
 
 
-provider "kubernetes" {
-  host                   = module.eks.endpoint
-  cluster_ca_certificate = base64decode(module.eks.certificate_authority)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      module.eks.name
-    ]
-  }
-}
-
 module "network" {
   source = "./network"
 }
