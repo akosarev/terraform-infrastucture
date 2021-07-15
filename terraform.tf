80b868eb6debefdae7a2d56dc788c87b8109a3d0
Artemiy Kosarev <artemiy.kosarev@klarna.com>
2021-07-15 13:38:42 +0200

    Change name for s3 bucket because previous name was busy

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "state-bucket"
+    bucket = "backend-state-terraform-infrastructure"
     key    = "infrastructure/terraform.tfstate"
     region = "eu-west-1"
   }
